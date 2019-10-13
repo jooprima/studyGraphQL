bfce4ac65834eb82807bd6d9d31e2a3f4d49676a
jooprima <jooprima@gmail.com>
2019-10-13 18:28:14 +0700

    create schema & type mahasiswa

--- schema/schema.js
+++ schema/schema.js
@@ -2,12 +2,24 @@ const graphql = require("graphql");
 
 const _ = require("lodash");
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt
+} = graphql;
 
 var jurusanx = [
   { jurusan: "Teknik Komputer", kaprodi: "Yohanes Prima", id: "1" },
   { jurusan: "Teknik Informatika", kaprodi: "Mahmuy", id: "2" },
   { jurusan: "Teknik Informasi", kaprodi: "Ucuo Marucup", id: "3" }
+];
+
+var data_mahasiswa = [
+  { nama: "agung", umur: 30, id: "1" },
+  { nama: "mawar", umur: 20, id: "2" },
+  { nama: "cemen", umur: 39, id: "3" }
 ];
 
 const JurusanType = new GraphQLObjectType({
@@ -19,6 +31,15 @@ const JurusanType = new GraphQLObjectType({
   })
 });
 
+const MahasiswaType = new GraphQLObjectType({
+  name: "mahasiswa",
+  fields: () => ({
+    id: { type: GraphQLID },
+    nama: { type: GraphQLString },
+    umur: { type: GraphQLInt }
+  })
+});
+
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
@@ -26,8 +47,15 @@ const RootQuery = new GraphQLObjectType({
       type: JurusanType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        console.log(typeof(args.id));
+        console.log(typeof args.id);
         return _.find(jurusanx, { id: args.id });
+      }
+    },
+    mahasiswa: {
+      type: MahasiswaType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(data_mahasiswa, { id: args.id });
       }
     }
   }
